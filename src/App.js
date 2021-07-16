7a5ea73cc522112a1477417afeff2891f959d28f
Ron Wise <ron@ronwisephoto.com>
2021-07-15 21:13:47 -0400

    adding token storage to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-// import './App.css';
 // import CreateTrip from "./components/CreateTrip"
 import Auth from './auth/Auth';
-// import Signup from './auth/Signup';
+import Signup from './auth/Signup';
 
+// import logo from './logo.svg';
+import './App.css';
+import TripsLandingPage from './components/TripsLandingPage'
+import CreateTrip from "./components/CreateTrip"
+import ViewTrips from "./components/ViewTrips"
 
 function App() {
+
+  const [sessionToken, setSessionToken] = useState('')
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'))
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken)
+    setSessionToken(newToken)
+    console.log(sessionToken)
+  }
+
   return (
     <div>
     <Auth />
-    {/* <CreateTrip /> */}
-  </div>
+
+{/* const clearToken = () => {
+   localStorage.clear()
+   setSessionToken('')
+ } */}
+
+      {/* <TripsLandingPage /> */}
+      {/* <CreateTrip /> */}
+      <ViewTrips />
+    </div>
   );
 }
 
-// const clearToken = () => {
-//   localStorage.clear()
-//   setSessionToken('')
-// }
-
 export default App;
+
