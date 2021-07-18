d2c0da59d75ad9eb2214727817b86636b5d029a5
Nellie Crocker <crocker.nellie@gmail.com>
2021-07-18 17:36:54 -0400

    Delete and Edit functionality working in the client- I need to figure out how to get it to work in the server as well.

---
+++ src/components/ReadOnlyRow.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from 'reactstrap'
+
+const ReadOnlyRow = ({ trip, handleEditClick, handleDeleteClick }) => {
+
+    return (
+            <tr>
+                <td>{trip.type}</td>
+                <td>{trip.country}</td>
+                <td>{trip.state}</td>
+                <td>{trip.city}</td>
+                <td>{trip.date}</td>
+                <td>{trip.details}</td>
+                <td><Button type="button" onClick={(event)=>handleEditClick(event, trip)}>Edit</Button>
+                <Button type="button" onClick={()=>handleDeleteClick(trip.id)}>Delete</Button></td>
+            </tr>
+    )
+}
+
+export default ReadOnlyRow
--- src/components/ViewTrips.js
+++ src/components/ViewTrips.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { Collapse, Button, CardBody, Card, Table } from 'reactstrap'
+import React, { useState, useEffect, Fragment } from 'react'
+import { Button, Table } from 'reactstrap'
 import EditTrip from "./EditTrip"
 import CreateTrip from './CreateTrip'
+import ReadOnlyRow from './ReadOnlyRow'
 
 
 const ViewTrips = () => {
-    //needed to edit trip
-    // const [updateActive, setUpdateActive] = useState(false)
-    // const [tripToUpdate, setTripToUpdate] = useState({})
 
-    const [tripData, setTripData] = useState([])
-    const accessToken = localStorage.getItem('sessionToken')
-    // const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImlhdCI6MTYyNjQ2MTQxMywiZXhwIjoxNjI2NTQ3ODEzfQ.jdaTbOtC3YcpvtPpD-_7vPdh4ELs0ZYJ9KNGEqyeSKQ"
-
+    const [tripsData, setTripsData] = useState([])
+    // const accessToken = localStorage.getItem('sessionToken')
+    const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImlhdCI6MTYyNjYzNDc5NywiZXhwIjoxNjI2NzIxMTk3fQ.cruo3ZGt-dEms5UtLnyxQ4Y9qvotA4TWLtbb7JehjS4"
 
     useEffect(() => {
         fetch('http://localhost:3000/trip/mytrips', {
@@ -23,66 +20,121 @@ const ViewTrips = () => {
             })
         })
             .then(res => res.json())
-            .then(json => setTripData(json))
+            .then(json => setTripsData(json))
     }, [accessToken])
 
 
-    //dropdown for details
-    const [isOpen, setIsOpen] = useState(false)
-    const toggle = () => setIsOpen(!isOpen)
 
-    //need to add this to remove console warning regarding unique key prop <ViewTrips tripData={trip} />
-    const tripList = tripData.map(trip => (
-        <div>
-            <Card>
-                <CardBody>
-                    <Table>
-                        <tbody>
-                            <tr>
-                                <th scope="row">{trip.type}</th>
-                                <td>{trip.country}</td>
-                                <td>{trip.state}</td>
-                                <td>{trip.city}</td>
-                                <td>{trip.date}</td>
-                                <td>{trip.details}</td>
-                                <td><Button onClick={EditTrip} color="secondary">Edit</Button>&nbsp;&nbsp;<Button onClick={EditTrip} color="secondary">Delete</Button></td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </CardBody>
-            </Card>
 
-        </div>
-    ))
+    //! INLINE ROW EDITING
+    const [editTripData, setEditTripData] = useState({
+        type: "",
+        country: "",
+        state: "",
+        city: "",
+        date: "",
+        details: ""
+    })
+    
+    //grabs the ID of the row you want to edit and assigns the new value to the 'name' attribute of that row
+    const [editTripId, setEditTripId] = useState(null)
+
+    const handleEditTripData = (event) => {
+        event.preventDefault()
+
+        const fieldName = event.target.getAttribute("name")
+        const fieldValue = event.target.value
+
+        const newTripData = { ...editTripData }
+        newTripData[fieldName] = fieldValue
+
+        setEditTripData(newTripData)
+    }
+    
+    //submits the edits made in the handleEditTripData function
+    const handleEditFormSubmit = (event) => {
+        event.preventDefault()
+
+        const editedTrip = {
+            id: editTripId,
+            type: editTripData.type,
+            country: editTripData.country,
+            state: editTripData.state,
+            city: editTripData.city,
+            date: editTripData.date,
+            details: editTripData.details
+        }
+
+        const newTripsData = [...tripsData]
+
+        const index = tripsData.findIndex((trip) => trip.id === editTripId)
+        newTripsData[index] = editedTrip
+
+        setTripsData(newTripsData)
+        setEditTripId(null)
+    }
+
+    //when the Edit button is clicked, determines which form fields are updated to which values
+    const handleEditClick = (event, trip) => {
+        event.preventDefault()
+        setEditTripId(trip.id)
+
+        const formValues = {
+            type: trip.type,
+            country: trip.country,
+            state: trip.state,
+            city: trip.city,
+            date: trip.date,
+            details: trip.details
+        }
+
+        setEditTripData(formValues)
+    }
+
+    //sets tripId to null when Cancel is clicked so that none of the trips are selected for editing
+    const handleCancelClick = () => {
+        setEditTripId(null)
+    }
+
+    //Deletes a trip from the table
+    const handleDeleteClick = (tripId) => {
+        const newTrip = [...tripsData]
+
+        const index = tripsData.findIndex((trip) => trip.id === tripId)
+
+        newTrip.splice(index, 1)
+
+        setTripsData(newTrip)
+    }
 
     return (
         <div>
-            <h2>Whoa! Check out all of the awesome trips you have, name! Have you event spent time in Location?</h2>
-            <Card>
-                <CardBody>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Type</th>
-                                <th>Country</th>
-                                <th>State</th>
-                                <th>City</th>
-                                <th>Date</th>
-                                <th>Details</th>
-                                <th>Modify</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            </tr>
-                            <tr>
-                                
-                            </tr>
-                        </tbody>
-                    </Table>
-                </CardBody>
-            </Card>
-            {tripList}
+            <h2>Whoa! Check out all of the awesome trips you have, name! Have you even spent time in Location?</h2>
+            <form onSubmit={handleEditFormSubmit}>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Country</th>
+                        <th>State</th>
+                        <th>City</th>
+                        <th>Date</th>
+                        <th>Details</th>
+                        <th>Modify</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tripsData.map((trip) =>
+                        <Fragment>
+                        {editTripId === trip.id
+                        ? (<EditTrip editTripData={editTripData} handleEditTripData={handleEditTripData} handleCancelClick={handleCancelClick} />)
+                        : (<ReadOnlyRow trip={trip} handleEditClick={handleEditClick} handleDeleteClick={handleDeleteClick} />)
+                        }
+                    </Fragment>
+                    )}
+                </tbody>
+            </Table>
+            </form>
             <div>
                 <Button onClick={CreateTrip} color="secondary">Create a New Trip</Button>
             </div>
